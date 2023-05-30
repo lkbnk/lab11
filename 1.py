264632e7a4a5fdfa2a233fee7f79af03afccc660
Ilya <yakubenko@complexis.biz>
2023-05-30 15:46:44 +0300

    task1 commit

--- 1.py
+++ 1.py
@@ -2,12 +2,12 @@ class restaurant:
     def __init__(self,name,type):
         self.restaurant_name = name
         self.cuisine_type = type
-    def d_restaurant(self):
-        print(" Название ресторана: ",self.restaurant_name,"\n Тип кухни:  ", self.cuisine_type)
-    def o_restaurant(self):
-        print(" Ресторан открыт")
+    def describe_restaurant(self):
+        print("Название ресторана: ",self.restaurant_name,"\nТип кухни:  ", self.cuisine_type)
+    def open_restaurant(self):
+        print("Ресторан открыт")
 
-newRestaurant = restaurant("Теремок", " Руская ")
+newRestaurant = restaurant("Теремок", "Руская")
 
-newRestaurant.d_restaurant()
-newRestaurant.o_restaurant()
+newRestaurant.describe_restaurant()
+newRestaurant.open_restaurant()
